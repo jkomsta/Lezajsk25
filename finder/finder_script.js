4eca19812d97f2f1104df6ca7d06165516d307ea
jkomsta <jkomsta@gmail.com>
2025-03-14 22:42:19 +0100

    finder2

--- finder/finder_script.js
+++ finder/finder_script.js
@@ -1,5 +1,5 @@
 
-        const API_KEY = '7e8e6d73f0c14664aa2b62fae033dd84'; // Zastąp swoim kluczem API Spoonacular
+        const API_KEY = '0259881f19404a89b4d30687846d8647'; // Zastąp swoim kluczem API Spoonacular
 
         // Funkcja do pobierania przepisów na podstawie składników
         async function findRecipes() {
@@ -31,7 +31,7 @@
 
             recipes.forEach(recipe => {
                 const recipeItem = document.createElement('div');
-                recipeItem.className = 'recipe-item';
+                recipeItem.className = 'recipe-item list-group-item list-group-item-action';
                 recipeItem.textContent = recipe.title;
                 recipeItem.onclick = () => getRecipeDetails(recipe.id);
                 recipeList.appendChild(recipeItem);
@@ -54,96 +54,43 @@
         function displayRecipeDetails(recipe) {
             const recipeDetails = document.getElementById('recipe-details');
             recipeDetails.innerHTML = `
-                <h2>${recipe.title}</h2>
+            <div class='container d-flex justify-content-center align-items-center flex-column'>
+                <h1>${recipe.title}</h1>
                 <img src="${recipe.image}" alt="${recipe.title}" style="max-width: 300px;">
-                <p><strong>Servings:</strong> ${recipe.servings}</p>
                 <p><strong>Ready in:</strong> ${recipe.readyInMinutes} minutes</p>
-                <h3>Ingredients:</h3>
-                <ul>
-                    ${recipe.extendedIngredients.map(ing => `<li>${ing.original}</li>`).join('')}
-                </ul>
-                <h3>Instructions:</h3>
-                <p>${recipe.instructions || "No instructions available."}</p>
+                <div class='d-flex justify-content-center align-items-stretch flex-row'>
+                    <div class='d-flex justify-content-center align-items-center flex-column ingredientlist'>
+                        <h2>Ingredients:</h2> <hr>
+                        <ul>
+                            ${recipe.extendedIngredients.map(ing => `<li>${ing.original}</li>`).join('')}
+                        </ul>
+                    </div>
+
+                    <div class='d-flex justify-content-center align-items-center flex-column ingredientlist'>
+                        <h3>Instructions:</h3>
+                        <p>${recipe.instructions || "No instructions available."}</p>
+                    </div>
+                </div>
+            </div>
             `;
         }
 
 
+        const text = "Recipe finder";
+        const speed = 60; // milliseconds per character
+        let i = 0;
 
-// ---------Responsive-navbar-active-animation-----------
-function test(){
-	var tabsNewAnim = $('#navbarSupportedContent');
-	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-	var activeItemNewAnim = tabsNewAnim.find('.active');
-	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-	var itemPosNewAnimTop = activeItemNewAnim.position();
-	var itemPosNewAnimLeft = activeItemNewAnim.position();
-	$(".hori-selector").css({
-		"top":itemPosNewAnimTop.top + "px",
-		"left":itemPosNewAnimLeft.left + "px",
-		"height": activeWidthNewAnimHeight + "px",
-		"width": activeWidthNewAnimWidth + "px"
-	});
-	$("#navbarSupportedContent").on("click","li",function(e){
-		$('#navbarSupportedContent ul li').removeClass("active");
-		$(this).addClass('active');
-		var activeWidthNewAnimHeight = $(this).innerHeight();
-		var activeWidthNewAnimWidth = $(this).innerWidth();
-		var itemPosNewAnimTop = $(this).position();
-		var itemPosNewAnimLeft = $(this).position();
-		$(".hori-selector").css({
-			"top":itemPosNewAnimTop.top + "px",
-			"left":itemPosNewAnimLeft.left + "px",
-			"height": activeWidthNewAnimHeight + "px",
-			"width": activeWidthNewAnimWidth + "px"
-		});
-	});
-}
-$(document).ready(function(){
-	setTimeout(function(){ test(); });
-});
-$(window).on('resize', function(){
-	setTimeout(function(){ test(); }, 500);
-});
-$(".navbar-toggler").click(function(){
-	$(".navbar-collapse").slideToggle(300);
-	setTimeout(function(){ test(); });
-});
+        function typeWriter() {
+            if (i < text.length) {
+                document.getElementById("text").innerHTML += text.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+        }
+
+        window.onload = typeWriter;
 
 
+        
+        
 
-// --------------add active class-on another-page move----------
-jQuery(document).ready(function($){
-	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
-
-	// Account for home page with empty path
-	if ( path == '' ) {
-		path = 'index.html';
-	}
-
-	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-	// Add active class to target link
-	target.parent().addClass('active');
-});
-
-
-
-
-// Add active class on another page linked
-// ==========================================
-// $(window).on('load',function () {
-//     var current = location.pathname;
-//     console.log(current);
-//     $('#navbarSupportedContent ul li a').each(function(){
-//         var $this = $(this);
-//         // if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.parent().addClass('active');
-//             $this.parents('.menu-submenu').addClass('show-dropdown');
-//             $this.parents('.menu-submenu').parent().addClass('active');
-//         }else{
-//             $this.parent().removeClass('active');
-//         }
-//     })
-// });
